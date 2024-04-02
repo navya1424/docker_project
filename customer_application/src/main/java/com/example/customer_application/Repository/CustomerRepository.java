c88b8a63b342ed44f1266778594da80c0fe93f93
navyasharma <ns14240612@gmail.com>
2024-04-02 23:26:39 +0530

    new changes

--- customer_application/src/main/java/com/example/customer_application/Repository/CustomerRepository.java
+++ customer_application/src/main/java/com/example/customer_application/Repository/CustomerRepository.java
@@ -1,10 +1,10 @@
-package com.example.customer_application.Repository;
-
-import com.example.customer_application.Entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
+//package com.example.customer_application.Repository;
+//
+//import com.example.customer_application.Entity.Customer;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface CustomerRepository extends JpaRepository<Customer, Long> {
+//
+//}
--- customer_application/src/main/java/com/example/customer_application/controller/Customer_Controller.java
+++ customer_application/src/main/java/com/example/customer_application/controller/Customer_Controller.java
@@ -1,7 +1,7 @@
 package com.example.customer_application.controller;
 
-import com.example.customer_application.Entity.Customer;
-import com.example.customer_application.service.CustomerService;
+//import com.example.customer_application.Entity.Customer;
+//import com.example.customer_application.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,35 +12,38 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/customers-dev")
 public class Customer_Controller {
-    private final CustomerService customerService;
+//    private final CustomerService customerService;
 
-    public Customer_Controller(CustomerService customerService) {
-        this.customerService = customerService;
-    }
-    @GetMapping("/all")
-    public ResponseEntity<List<Customer>> getAllCustomers() {
-        List<Customer> customers = customerService.getAll();
-        return ResponseEntity.ok(customers);
-    }
+//    public Customer_Controller(CustomerService customerService) {
+//        this.customerService = customerService;
+//    }
+//    @GetMapping("/all")
+//    public ResponseEntity<List<Customer>> getAllCustomers() {
+//        List<Customer> customers = customerService.getAll();
+//        return ResponseEntity.ok(customers);
+//    }
+//
+//    @GetMapping("/get/{id}")
+//    public ResponseEntity<Optional<Customer>> getCustomerById(@PathVariable Long id){
+//        return ResponseEntity.ok(customerService.getById(id));
+//    }
+//
+//    @PostMapping("/add")
+//    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+//        return ResponseEntity.ok(customerService.addCustomer(customer));
+//    }
+//
+//    @PutMapping("/put/{id}")
+//    public ResponseEntity <Customer> updateCustomer(@RequestBody Customer customer, @PathVariable Long id) {
+//        return ResponseEntity.ok(customerService.updateCustomer(customer,id));
+//    }
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<Optional<Customer>> getCustomerById(@PathVariable Long id){
-        return ResponseEntity.ok(customerService.getById(id));
-    }
-
-    @PostMapping("/add")
-    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
-        return ResponseEntity.ok(customerService.addCustomer(customer));
-    }
-
-    @PutMapping("/put/{id}")
-    public ResponseEntity <Customer> updateCustomer(@RequestBody Customer customer, @PathVariable Long id) {
-        return ResponseEntity.ok(customerService.updateCustomer(customer,id));
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteCustomer(@PathVariable Long id){
-        customerService.deletedById(id);
+//    @DeleteMapping("/delete/{id}")
+    @GetMapping("all")
+    public ResponseEntity<String> deleteCustomer(
+//            @PathVariable Long id
+    ){
+//        customerService.deletedById(id);
         return ResponseEntity.ok().body("Customer deleted successfully");
     }
 }
